2e836758f5f8621aaf0385939b04c4323d4ba3f8
Yeasin Arafath <arafath.yeasin1019@gmail.com>
2023-07-30 02:04:44 +0600

    feat: Implemented Next Image and Home page, about page and contact page UI

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React from 'react';
+import styles from "./page.module.css";
+import Image from 'next/image';
+import Button from '@/components/Button/Button';
 
 function About() {
   return (
-    <div>About</div>
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image className={styles.img} src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" fill={true} alt='' />
+        <div className={styles.imgText}>
+          <h1 className={styles.imgTitle}>Digital Storytellers</h1>
+          <h2 className={styles.imgDesc}>Handcrafting award winning digital experiences</h2>
+        </div>
+      </div>
+      <div className={styles.textContainer}>
+        <div className={styles.item}>
+          <h1 className={styles.title}>Who Are We?</h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa doloremque incidunt vel sit et. Minus ad error ipsa modi totam ducimus debitis cum quas, laudantium, delectus, sequi ut porro optio?
+            <br />
+            <br />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos nostrum quaerat quidem aperiam quas minima iure veniam, minus maxime debitis facilis dolorum suscipit vitae tenetur quae explicabo ab tempora esse?
+            <br />
+            <br />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero pariatur deleniti ex nobis autem, quaerat veniam iure obcaecati magnam temporibus velit alias. Officia saepe quae explicabo laborum amet quia accusamus!
+          </p>
+        </div>
+        <div className={styles.item}>
+          <h1 className={styles.title}>What We Do</h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi fugiat laborum commodi laudantium. Quaerat inventore, distinctio adipisci illum eius repellendus quae commodi provident nam facere excepturi pariatur eveniet delectus repudiandae?
+            <br />
+            <br />
+            - Dynamic Websites
+            <br />
+            <br />
+            - Fast and Handy
+            <br />
+            <br />
+            - Mobile Apps
+          </p>
+          <Button url="/contact" text="Contact" />
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import styles from "./footer.module.css";
+import Image from 'next/image';
 
 function Footer() {
   return (
     <div className={styles.container}>
         <div>©2023 Lamamia. All rights reserved.</div>
-        <div></div>
+        <div className={styles.social}>
+          <Image className={styles.icon} src="/1.png" alt='Facebook image' width={15} height={15} />
+          <Image className={styles.icon} src="/2.png" alt='Instagram image' width={15} height={15} />
+          <Image className={styles.icon} src="/3.png" alt='Twitter image' width={15} height={15} />
+          <Image className={styles.icon} src="/4.png" alt='Youtube image' width={15} height={15} />
+        </div>
     </div>
   )
 }
